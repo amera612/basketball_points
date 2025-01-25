8283065e715ac2fc554d44affe359a58d0e83451
Your Name <you@example.com>
2025-01-25 21:56:00 +0200

    edit in gradle

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(PointsCounter());
+  runApp(const PointsCounter());
 }
 
 class PointsCounter extends StatelessWidget {
-  PointsCounter({super.key});
+  const PointsCounter({super.key});
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => CounterCubit(),
-      child: const MaterialApp(
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomePage(),
       ),
--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:basketball_points/constant.dart';
+import 'package:basketball_points/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,7 +10,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.orangeAccent,
+        backgroundColor: kPrimary,
         title: const Text("Points counter"),
       ),
       body: Column(
@@ -27,19 +29,21 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    '0',
+                    '${BlocProvider.of<CounterCubit>(context).teamAPoints}',
                     style: const TextStyle(
                       fontSize: 160,
                     ),
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orangeAccent,
+                      backgroundColor: kPrimary,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(0),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).TeamAPoint(1);
+                    },
                     child: const Text(
                       'Add 1 point',
                       style: TextStyle(
@@ -52,12 +56,14 @@ class HomePage extends StatelessWidget {
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orangeAccent,
+                      backgroundColor: kPrimary,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(0),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).TeamAPoint(2);
+                    },
                     child: const Text(
                       'Add 2 point',
                       style: TextStyle(
@@ -70,12 +76,14 @@ class HomePage extends StatelessWidget {
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orangeAccent,
+                      backgroundColor: kPrimary,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(0),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).TeamAPoint(3);
+                    },
                     child: const Text(
                       'Add 3 point',
                       style: TextStyle(
@@ -101,19 +109,21 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    '0',
+                    '${BlocProvider.of<CounterCubit>(context).teamBPoints}',
                     style: const TextStyle(
                       fontSize: 160,
                     ),
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orangeAccent,
+                      backgroundColor: kPrimary,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(0),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).TeamBPoint(1);
+                    },
                     child: const Text(
                       'Add 1 point',
                       style: TextStyle(
@@ -126,12 +136,14 @@ class HomePage extends StatelessWidget {
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orangeAccent,
+                      backgroundColor: kPrimary,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(0),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).TeamBPoint(2);
+                    },
                     child: const Text(
                       'Add 2 point',
                       style: TextStyle(
@@ -144,12 +156,14 @@ class HomePage extends StatelessWidget {
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orangeAccent,
+                      backgroundColor: kPrimary,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(0),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).TeamBPoint(3);
+                    },
                     child: const Text(
                       'Add 3 point',
                       style: TextStyle(
@@ -163,12 +177,15 @@ class HomePage extends StatelessWidget {
           ),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.orangeAccent,
+              backgroundColor: kPrimary,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(0),
               ),
             ),
-            onPressed: () {},
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).teamBPoints = 0;
+              BlocProvider.of<CounterCubit>(context).teamAPoints = 0;
+            },
             child: const Text(
               'reset',
               style: TextStyle(
